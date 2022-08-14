41c89bdb5e482a14105f91d12fc70987ddd77a0e
Raptor2k1 <87739732+Raptor2k1@users.noreply.github.com>
2022-08-13 18:52:28 -0700

    Changed data structures
    Devices are now objects and not represented by ordered lists of attributes.

--- main.py
+++ main.py
@@ -7,6 +7,47 @@
 import subprocess
 
 
+class InterfaceDevice:
+    """
+    Represents a controller device tied to the computer. Contains data members for the device Instance ID, short ID,
+    description, and OEM name (all stored as strings). All initialized as empty strings, until they are set.
+
+    Contains methods to set and get all data members.
+    """
+
+    def __init__(self):
+        self._instance_id = ""
+        self._short_id = ""
+        self._description = ""
+        self._oem_name = ""
+
+    # Set methods for all data members of a device object
+    def set_isntance_id(self, instance_id: str):
+        self._instance_id = instance_id
+
+    def set_short_id(self, short_id: str):
+        self._short_id = short_id
+
+    def set_description(self, description: str):
+        self._description = description
+
+    def set_oem_name(self, oem_name: str):
+        self._oem_name = oem_name
+
+    # Get methods for the device object's data members
+    def get_isntance_id(self):
+        return self._instance_id
+
+    def get_short_id(self):
+        return self._short_id
+
+    def get_description(self):
+        return self._description
+
+    def get_oem_name(self):
+        return self._oem_name
+
+
 class ComputerDevices:
     """
     Represents the controller devices plugged into the computer. Contains a dictionary of devices keyed to their
@@ -34,23 +75,33 @@ class ComputerDevices:
         # subprocess.run('ControllerCheck.bat')                                # If running off external .bat
 
         # Log device information to ControllerCheck.txt and bring it back in so that we can parse it
-        subprocess.run("pnputil /enum-devices /class HIDclass > %~dp0\ControllerCheck.txt")
+        subprocess.run("pnputil /enum-devices /class HIDclass > %~dp0ControllerCheck.txt")
         controller_summary = open('ControllerCheck.txt')                       # OPEN CONTROLLER SUMMARY TXT
         current_device = None
         for line in controller_summary:
             if "Instance ID" in line:
-                device = self._parse_device_check_line(line)
-                self._device_dict[device] = [None, None, None, None]
-                self._device_dict[device][0] = self._parse_for_short_id(device)
-                self._device_dict[device][3] = self._get_name(self._device_dict[device][0])
-                current_device = device
-            if "Status:" in line:
+                device = self._parse_device_check_line(line)                    # Create object keyed to instance ID
+                self._device_dict[device] = InterfaceDevice()                   # Set instance ID and short ID
+                self._device_dict[device].set_instance_id(device)
+                self._device_dict[device].set_short_id(self._parse_for_short_id(device))
+            if "Status:" in line:                                               # Set status
                 status = self._parse_device_check_line(line)
-                self._device_dict[current_device][1] = status
-            if "Device Description:" in line:
+                self._device_dict[device].set_status(status)
+            if "Device Description:" in line:                                   # Set description
                 description = self._parse_device_check_line(line)
-                self._device_dict[current_device][2] = description
+                self._device_dict[device].set_description(description)
         controller_summary.close()                                              # CLOSE CONTROLLER SUMMARY TXT
+
+        # Clear non-controller devices from the dictionary
+        for device in self._device_dict:
+            if device.get_description != "HID-compliant game controller":
+                del device
+            # May change index due to deleting in-place... try using pop if it doesn't work
+            # or adding non-controllers to list of non-controller keys, then in next iteration delete all non-controllers
+
+        # Get OEM Names after clearing all the non-controllers from the dictionary to reduce errors/not wase cycles
+        for device in self._device_dict:
+            self._device_dict[device].set_oem_name = (self._get_name(device.get_short_id))
 
     def disable_controllers(self):
         """
@@ -89,28 +140,38 @@ class ComputerDevices:
         Returns the OEM/Full name of the hardware device in question.
         """
 
-        oem_name_query = "reg query HKEY_CURRENT_USER\System\CurrentControlSet\Control\MediaProperties\
-                            \PrivateProperties\Joystick\OEM\\" + short_id + " /v OEMName > oem_name.txt"
+        oem_name_query = 'reg query "HKEY_CURRENT_USER\System\CurrentControlSet\Control\MediaProperties\
+                            \PrivateProperties\Joystick\OEM\\' + short_id + '" /v OEMName > %~dp0oem_name.txt'
         subprocess.run(oem_name_query)
-        oem_name_txt = open(r'ControllerCheck.txt')
-        for line in oem_name_txt:
-            if "OEMName" in line:
-                char_index = 0
-                oem_name = ""
-                while line[char_index] is not None:                                 # stops parsing once out of readable chars
-                    while line[char_index] == " ":                                  # Advance past initial spaces
-                        char_index += 1
-                    while line[char_index] != " ":                                  # Advance past OEMname
-                        char_index =+ 1
-                    while line[char_index] == " ":                                  # Advance past spaces
-                        char_index += 1
-                    while line[char_index] != " ":                                  # Advance past reg_sz
-                        char_index =+ 1
-                    while line[char_index] == " ":                                  # Advance past final spaces
-                        char_index += 1
-                    oem_name = oem_name + line[char_index]                          # Transcribe
-                    char_index += 1
-                    # IF first two terms/spaces are always set.... maybe just advance a set number of spaces? Also maybe just move it to line 3 (file index of 2) by default?
+        oem_name_txt = open('ControllerCheck.txt')
+        target_line = oem_name_txt[2]
+        char_index = 26                                             # Start transcibing from first char index of name
+        oem_name = ""
+        while target_line[char_index] is not None:                  # Transcribe until chars stop
+            oem_name = oem_name + line[char_index]
+            char_index += 1
+        # NEED TO HANDLE ERRORS/NO RETURNS - WHAT IS RETURN VALUE OF VALUE NOT FOUND?
+
+
+                # ORIGINAL DRAFT METHOD - MAY KEEP IF START CHAR IS NOT STATIC - BUT GO STRAIGHT TO TARGET LINE INSTEAD OF FOR LOOP
+                # for line in oem_name_txt:
+                #     if "OEMName" in line:
+                #         char_index = 0
+                #         oem_name = ""
+                #         while line[char_index] == " ":  # Advance past initial spaces
+                #             char_index += 1
+                #         while line[char_index] != " ":  # Advance past OEMname
+                #             char_index = + 1
+                #         while line[char_index] == " ":  # Advance past spaces
+                #             char_index += 1
+                #         while line[char_index] != " ":  # Advance past reg_sz
+                #             char_index = + 1
+                #         while line[char_index] == " ":  # Advance past final spaces
+                #             char_index += 1
+                #         while line[char_index] is not None:  # Transcribe until chars stop
+                #             oem_name = oem_name + line[char_index]
+                #             char_index += 1
+
         oem_name_txt.close()
         return oem_name
 
@@ -132,8 +193,8 @@ class ComputerDevices:
         char_index += 1                                             # Move character to first space after colon
         while line[char_index] == " ":                              # Move character to first char after empty space
             char_index += 1
-        while line[char_index] is not None or line[char_index] != " ":
-            device_id = device_id + line[char_index]                # Transcribe chars until they stop existing
+        while line[char_index] is not None:                         # Transcribe chars until they stop existing
+            device_id = device_id + line[char_index]
             char_index += 1
         return device_id
 
@@ -160,13 +221,36 @@ class ComputerDevices:
         return device_id                                            # Return the short ID for the device for use in
                                                                     # retrieving the device's exact name.
 
+
+
+
+
+
+
+
+
 # CURRENT EXPECTED ISSUES
 # Probably need to revamp parsing stop points / last lines
 # May be able to greatly simplify parsing of OEMName - check if the actual name always starts at the same char index
+    # May be index 24, 25, or 26?
 # Check syntax for \ within a quote to make sure they're used as expected
 # Need to add actual commands/scripts to run these when starting the app (e.g. prompt for enable/disable/check on boot)
 # Make sure methods are located where appropriate
 # Always do a check to build dictionary on program boot
+# ENABLE/DISABLE are going to need admin privledges... or just make sure core script is run as admin
+# Probably need to make batch file to call the script that can be run as admin
+# MAKE SURE X-Box controllers aren't being turned off too
+# For now: only turn off "HID-compliant game controller" description
+    # helper method to find last char in string if getting index errors or infinite loops with parsing?
+# FUTURE METHODS:
+    # Display device summaries (for each dict entry show device name + info)
+    # Mod to check summary - if error with getting name, just make description + short ID the name
+    # Disable specific device command
+    # Enable specific device command
+# clean up spaces at the end of transcribed chunks (any spaces after the last actual char)
+# **NOT EVERYTHING IN HID IS A JOYSTICK IN THE REGISTRY
+# Make sure txt saving actually saves to directory with .py file and not somewhere else... may need to tweak
+# MAKE EACH DEVICE AN OBJECT vs a list of traits (get/sets for long name, short name, status, desc)
 
 
 # THE PLAN
